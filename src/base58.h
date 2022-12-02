14b573416478bf1953b0fcd640cf1787cf6b7979
Ruckard Smith <ruckard.smith@gmail.com>
2022-12-02 22:11:21 +0100

    FORK-BADGERX11: [PUBKEY-ADDRESS] X11Coin: address begin with 'X'

--- src/base58.h
+++ src/base58.h
@@ -276,7 +276,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 25,  // Badgercoin: address begin with 'M'
+        PUBKEY_ADDRESS = 75,  // X11Coin: address begin with 'X'
         SCRIPT_ADDRESS = 8, 
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
