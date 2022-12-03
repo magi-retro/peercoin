d644fe2d7bdee02ed187166082c139001c0a1559
Ruckard Smith <ruckard.smith@gmail.com>
2022-12-03 21:43:58 +0100

    FORK-BADGER: [REMOVE] [REPLACE-COINNAME] Replace from WhiteCoin to BadgerCoin . (2)

--- src/util.cpp
+++ src/util.cpp
@@ -1006,7 +1006,7 @@ static std::string FormatException(std::exception* pex, const char* pszThread)
     char pszModule[MAX_PATH] = "";
     GetModuleFileNameA(NULL, pszModule, sizeof(pszModule));
 #else
-    const char* pszModule = "Whitecoin";
+    const char* pszModule = "Badgercoin";
 #endif
     if (pex)
         return strprintf(
@@ -1055,13 +1055,13 @@ void PrintExceptionContinue(std::exception* pex, const char* pszThread)
 boost::filesystem::path GetDefaultDataDir()
 {
     namespace fs = boost::filesystem;
-    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Whitecoin
-    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Whitecoin
-    // Mac: ~/Library/Application Support/Whitecoin
-    // Unix: ~/.Whitecoin
+    // Windows < Vista: C:\Documents and Settings\Username\Application Data\Badgercoin
+    // Windows >= Vista: C:\Users\Username\AppData\Roaming\Badgercoin
+    // Mac: ~/Library/Application Support/Badgercoin
+    // Unix: ~/.Badgercoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Whitecoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "Badgercoin";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -1073,10 +1073,10 @@ boost::filesystem::path GetDefaultDataDir()
     // Mac
     pathRet /= "Library/Application Support";
     fs::create_directory(pathRet);
-    return pathRet / "Whitecoin";
+    return pathRet / "Badgercoin";
 #else
     // Unix
-    return pathRet / ".Whitecoin";
+    return pathRet / ".Badgercoin";
 #endif
 #endif
 }
@@ -1118,7 +1118,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "Whitecoin.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "Badgercoin.conf"));
     if (!pathConfigFile.is_complete()) pathConfigFile = GetDataDir(false) / pathConfigFile;
     return pathConfigFile;
 }
@@ -1149,7 +1149,7 @@ void ReadConfigFile(map<string, string>& mapSettingsRet,
 
 boost::filesystem::path GetPidFile()
 {
-    boost::filesystem::path pathPidFile(GetArg("-pid", "Whitecoind.pid"));
+    boost::filesystem::path pathPidFile(GetArg("-pid", "Badgercoind.pid"));
     if (!pathPidFile.is_complete()) pathPidFile = GetDataDir() / pathPidFile;
     return pathPidFile;
 }
@@ -1289,10 +1289,10 @@ void AddTimeData(const CNetAddr& ip, int64 nTime)
                 if (!fMatch)
                 {
                     fDone = true;
-                    string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong Whitecoin will not work properly.");
+                    string strMessage = _("Warning: Please check that your computer's date and time are correct! If your clock is wrong Badgercoin will not work properly.");
                     strMiscWarning = strMessage;
                     printf("*** %s\n", strMessage.c_str());
-                    uiInterface.ThreadSafeMessageBox(strMessage+" ", string("Whitecoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION);
+                    uiInterface.ThreadSafeMessageBox(strMessage+" ", string("Badgercoin"), CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION);
                 }
             }
         }
