5a7d9c5be468afc096cdfeaa8fc5ee01995480a1
Ruckard Smith <ruckard.smith@gmail.com>
2022-12-06 19:48:48 +0100

    FORK-MINT: [REPLACE-COINNAME] Replace GrainCoin to MintCoin. (2)

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,7 @@ map<uint256, map<uint256, CDataStream*> > mapOrphanTransactionsByPrev;
 // Constant stuff for coinbase transactions we create:
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "Grain Signed Message:\n";
+const string strMessageMagic = "MintCoin Signed Message:\n";
 
 double dHashesPerSec;
 int64 nHPSTimerStart;
@@ -1542,8 +1542,8 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex, bool fJustCheck)
     // Now that the whole chain is irreversibly beyond that time it is applied to all blocks except the
     // two in the chain that violate it. This prevents exploiting the issue against nodes in their
     // initial block download.
-    bool fEnforceBIP30 = true; // Always active in Grain
-    bool fStrictPayToScriptHash = true; // Always active in Grain
+    bool fEnforceBIP30 = true; // Always active in MintCoin
+    bool fStrictPayToScriptHash = true; // Always active in MintCoin
 
     //// issue here: it doesn't know the version
     unsigned int nTxPos;
@@ -2491,7 +2491,7 @@ bool CheckDiskSpace(uint64 nAdditionalBytes)
         string strMessage = _("Warning: Disk space is low!");
         strMiscWarning = strMessage;
         printf("*** %s\n", strMessage.c_str());
-        uiInterface.ThreadSafeMessageBox(strMessage, "Grain", CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
+        uiInterface.ThreadSafeMessageBox(strMessage, "MintCoin", CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
         StartShutdown();
         return false;
     }
