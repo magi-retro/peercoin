8ddfd11322b80ff2e6b30ae1f4e9ff46c2351b52
Ruckard Smith <ruckard.smith@gmail.com>
2022-12-07 22:33:36 +0100

    FORK-GRAIN: [REMOVE] [PUBKEY-ADDRESS] Pubkey address was updated for GrainCoin.

--- src/base58.h
+++ src/base58.h
@@ -276,7 +276,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 34,  // BottleCaps: address begin with 'b'
+        PUBKEY_ADDRESS = 20,  // Grain: address begin with '9'
         SCRIPT_ADDRESS = 8, 
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
