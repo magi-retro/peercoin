d4fdc713d834afb36459cc7ea076dd3224250ba4
Ruckard Smith <ruckard.smith@gmail.com>
2022-12-10 10:05:03 +0100

    FORK-MAGI: [REMOVE] [CHANGE-PORT]
    -"Listen for JSON-RPC connections on <port> (default: 8344 or testnet: 18344)"),
    +"Listen for JSON-RPC connections on <port> (default: 8232 or testnet: 18232)"),
    
    -QT_TRANSLATE_NOOP("bitcoin-core", "Listen for connections on <port> (default: 23489 or testnet: 22788)"),
    +QT_TRANSLATE_NOOP("bitcoin-core", "Listen for connections on <port> (default: 8233 or testnet: 18233)"),

--- src/qt/bitcoinstrings.cpp
+++ src/qt/bitcoinstrings.cpp
@@ -44,7 +44,7 @@ QT_TRANSLATE_NOOP("bitcoin-core", ""
 "Execute command when the best block changes (%s in cmd is replaced by block "
 "hash)"),
 QT_TRANSLATE_NOOP("bitcoin-core", ""
-"Listen for JSON-RPC connections on <port> (default: 8344 or testnet: 18344)"),
+"Listen for JSON-RPC connections on <port> (default: 8232 or testnet: 18232)"),
 QT_TRANSLATE_NOOP("bitcoin-core", ""
 "Number of seconds to keep misbehaving peers from reconnecting (default: "
 "86400)"),
@@ -116,7 +116,7 @@ QT_TRANSLATE_NOOP("bitcoin-core", "Invalid amount for -paytxfee=<amount>: '%s'")
 QT_TRANSLATE_NOOP("bitcoin-core", "Invalid amount for -reservebalance=<amount>"),
 QT_TRANSLATE_NOOP("bitcoin-core", "Invalid amount"),
 QT_TRANSLATE_NOOP("bitcoin-core", "List commands"),
-QT_TRANSLATE_NOOP("bitcoin-core", "Listen for connections on <port> (default: 23489 or testnet: 22788)"),
+QT_TRANSLATE_NOOP("bitcoin-core", "Listen for connections on <port> (default: 8233 or testnet: 18233)"),
 QT_TRANSLATE_NOOP("bitcoin-core", "Loading addresses..."),
 QT_TRANSLATE_NOOP("bitcoin-core", "Loading block index..."),
 QT_TRANSLATE_NOOP("bitcoin-core", "Loading wallet..."),
