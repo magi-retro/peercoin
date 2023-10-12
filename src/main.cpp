8611cf6cad29c677bdb355777567c9cd79416e42
Ruckard Smith <ruckard.smith@gmail.com>
2023-10-12 21:00:08 +0200

    FORK-MAGI: main.cpp: Remove no longer used generateMTRandom function.

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,6 @@
 #include <boost/algorithm/string/replace.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/filesystem/fstream.hpp>
-#include <boost/random/mersenne_twister.hpp>
-#include <boost/random/uniform_int_distribution.hpp>
 
 using namespace std;
 using namespace boost;
@@ -927,13 +925,6 @@ uint256 WantedByOrphan(const CBlock* pblockOrphan)
     return pblockOrphan->hashPrevBlock;
 }
 
-
-int generateMTRandom(unsigned int s, int range)
-{
-	random::mt19937 gen(s);
-    random::uniform_int_distribution<> dist(0, range);
-    return dist(gen);
-}
 
 
 
