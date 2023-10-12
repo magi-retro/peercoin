000d9fb5d22d713ef7eba1678834d6d3b373a3dc
Ruckard Smith <ruckard.smith@gmail.com>
2023-10-12 20:17:04 +0200

    FORK-MAGI: Adapt to magi. version.cpp: GIT_COMMIT_DATE is updated.

--- src/version.cpp
+++ src/version.cpp
@@ -37,7 +37,7 @@ const std::string CLIENT_NAME("Magi");
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
 #    define GIT_COMMIT_ID ""
-#    define GIT_COMMIT_DATE "1398432165"
+#    define GIT_COMMIT_DATE "1408381731"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
