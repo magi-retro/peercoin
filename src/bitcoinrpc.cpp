fc6af4841063f7fa58fb01ab9b163919b3e2207c
Ruckard Smith <ruckard.smith@gmail.com>
2023-10-12 18:43:11 +0200

    FORK-MAGI: [REMOVE] [CHANGE-PORT] (2)

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -43,7 +43,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 22347 : 32347;
+    return GetBoolArg("-testnet", false) ? 19232 : 8232;
 }
 
 Object JSONRPCError(int code, const string& message)
