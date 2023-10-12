91eb6d3ecc65999dad85e07bba3ef4d503f601e0
Ruckard Smith <ruckard.smith@gmail.com>
2023-10-12 19:16:40 +0200

    FORK-MAGI: src/util.h . Add fDebugMagi and GetTimeOffset() .

--- src/util.h
+++ src/util.h
@@ -143,6 +143,7 @@ inline void Sleep(int64 n)
 extern std::map<std::string, std::string> mapArgs;
 extern std::map<std::string, std::vector<std::string> > mapMultiArgs;
 extern bool fDebug;
+extern bool fDebugMagi;
 extern bool fDebugNet;
 extern bool fPrintToConsole;
 extern bool fPrintToDebugger;
@@ -227,6 +228,7 @@ uint256 GetRandHash();
 int64 GetTime();
 void SetMockTime(int64 nMockTimeIn);
 int64 GetAdjustedTime();
+int64_t GetTimeOffset();
 long hex2long(const char* hexString);
 std::string FormatFullVersion();
 std::string FormatSubVersion(const std::string& name, int nClientVersion, const std::vector<std::string>& comments);
