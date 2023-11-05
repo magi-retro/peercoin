0235451cfcdd899aa00573c12765c56788e1d461
Ruckard Smith <ruckard.smith@gmail.com>
2023-11-05 19:24:09 +0100

    FORK-MAGI: [REMOVE] [REPLACE-COINNAME] (5)

--- src/main.cpp
+++ src/main.cpp
@@ -71,7 +71,7 @@ map<uint256, map<uint256, CDataStream*> > mapOrphanTransactionsByPrev;
 // Constant stuff for coinbase transactions we create:
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "X11Coin Signed Message:\n";
+const string strMessageMagic = "Magi Signed Message:\n";
 
 double dHashesPerSec;
 int64 nHPSTimerStart;
@@ -1582,8 +1582,8 @@ bool CBlock::ConnectBlock(CTxDB& txdb, CBlockIndex* pindex, bool fJustCheck)
     // Now that the whole chain is irreversibly beyond that time it is applied to all blocks except the
     // two in the chain that violate it. This prevents exploiting the issue against nodes in their
     // initial block download.
-    bool fEnforceBIP30 = true; // Always active in X11Coin
-    bool fStrictPayToScriptHash = true; // Always active in X11Coin
+    bool fEnforceBIP30 = true; // Always active in Magi
+    bool fStrictPayToScriptHash = true; // Always active in Magi
 
     //// issue here: it doesn't know the version
     unsigned int nTxPos;
@@ -2571,7 +2571,7 @@ bool CheckDiskSpace(uint64 nAdditionalBytes)
         string strMessage = _("Warning: Disk space is low!");
         strMiscWarning = strMessage;
         printf("*** %s\n", strMessage.c_str());
-        uiInterface.ThreadSafeMessageBox(strMessage, "X11Coin", CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
+        uiInterface.ThreadSafeMessageBox(strMessage, "Magi", CClientUIInterface::OK | CClientUIInterface::ICON_EXCLAMATION | CClientUIInterface::MODAL);
         StartShutdown();
         return false;
     }
