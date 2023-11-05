8ba041af12aa78ecaad61dc197303df77fc84153
Ruckard Smith <ruckard.smith@gmail.com>
2023-11-05 19:22:05 +0100

    FORK-MAGI: [ADAPT] getblocks command limit is 500 instead of 1000.

--- src/main.cpp
+++ src/main.cpp
@@ -3356,7 +3356,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         // Send the rest of the chain
         if (pindex)
             pindex = pindex->pnext;
-        int nLimit = 1000;
+        int nLimit = 500;
         printf("getblocks %d to %s limit %d\n", (pindex ? pindex->nHeight : -1), hashStop.ToString().substr(0,20).c_str(), nLimit);
         for (; pindex; pindex = pindex->pnext)
         {
