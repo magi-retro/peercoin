b7204c26cd9dbeeb1131387e94816ecab5ffa71c
Ruckard Smith <ruckard.smith@gmail.com>
2023-11-11 20:17:31 +0100

    FORK-MAGI: [ADAPT] getmininginfo object . (8)

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -85,7 +85,7 @@ Value getmininginfo(const Array& params, bool fHelp)
     obj.push_back(Pair("blockvalue",       (uint64_t)(GetProofOfWorkReward(pindexBest->nBits, pindexBest->nHeight, 0)/COIN)));
     obj.push_back(Pair("netmhashps",       GetPoWMHashPS()));
     obj.push_back(Pair("netstakeweight",   GetPoSKernelPS()));
-    obj.push_back(Pair("errors",        GetWarnings("statusbar")));
+    obj.push_back(Pair("errors",           GetWarnings("statusbar")));
     obj.push_back(Pair("generate",      GetBoolArg("-gen")));
     obj.push_back(Pair("genproclimit",  (int)GetArg("-genproclimit", -1)));
     obj.push_back(Pair("hashespersec",  gethashespersec(params, false)));
